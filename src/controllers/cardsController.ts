1996d51469880949f0c8144e94f03a93e540a510
cassandra-mn <arthenciac@gmail.com>
2022-07-09 15:39:49 -0300

    feat: validate employed

--- src/controllers/cardsController.ts
+++ src/controllers/cardsController.ts
@@ -3,9 +3,10 @@ import {Request, Response} from 'express';
 import * as cardsService from '../services/cardsService.js';
 
 export async function createCard(req: Request, res: Response) {
-    const {type}: {type: string} = req.body;
+    const {type, id}: {type: string, id: number} = req.body;
 
     cardsService.validateType(type);
+    cardsService.validateEmployee(id);
     
     res.sendStatus(201);
 }
--- src/services/cardsService.ts
+++ src/services/cardsService.ts
@@ -1,5 +1,12 @@
+import * as employee from '../repositories/employeeRepository.js';
+
 export async function validateType(type: string) {
     const validTypes = ['groceries', 'restaurants', 'transport', 'education', 'health'];
     const isValid = validTypes.some(validType => type === validType);
     if (!isValid) throw {status: 422};
+}
+
+export async function validateEmployee(id: number) {
+    const isRegistered = employee.findById(id);
+    if (!isRegistered) throw {status: 422};
 }
