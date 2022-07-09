0490750480fd77f1af2c7ede3c20f2c8bd8e7903
cassandra-mn <arthenciac@gmail.com>
2022-07-09 15:59:49 -0300

    feat: validate type by employed

--- src/controllers/cardsController.ts
+++ src/controllers/cardsController.ts
@@ -3,10 +3,11 @@ import {Request, Response} from 'express';
 import * as cardsService from '../services/cardsService.js';
 
 export async function createCard(req: Request, res: Response) {
-    const {type, id}: {type: string, id: number} = req.body;
+    const {type, id}: {type: any, id: number} = req.body;
 
     cardsService.validateType(type);
     cardsService.validateEmployee(id);
+    cardsService.validateTypeByEmployee(type, id);
     
     res.sendStatus(201);
 }
--- src/services/cardsService.ts
+++ src/services/cardsService.ts
@@ -1,4 +1,7 @@
+import * as card from '../repositories/cardRepository.js';
 import * as employee from '../repositories/employeeRepository.js';
+
+import {TransactionTypes} from '../repositories/cardRepository.js';
 
 export async function validateType(type: string) {
     const validTypes = ['groceries', 'restaurants', 'transport', 'education', 'health'];
@@ -9,4 +12,9 @@ export async function validateType(type: string) {
 export async function validateEmployee(id: number) {
     const isRegistered = employee.findById(id);
     if (!isRegistered) throw {status: 422};
+}
+
+export async function validateTypeByEmployee(type: TransactionTypes, id: number) {
+    const haveCard = card.findByTypeAndEmployeeId(type, id);
+    if (haveCard) return {status: 409};
 }
