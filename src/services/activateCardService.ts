518a664a1edc6319b96220c99e32b1cb1abb334d
cassandra-mn <arthenciac@gmail.com>
2022-07-10 13:15:43 -0300

    feat: validate cvc

--- src/services/activateCardService.ts
+++ src/services/activateCardService.ts
@@ -1,4 +1,6 @@
 import dayjs from 'dayjs';
+import Cryptr from 'cryptr';
+const cryptr = new Cryptr('secret');
 
 import * as cards from '../repositories/cardRepository.js';
 
@@ -20,8 +22,15 @@ export async function validateActivation(card: any) {
     if (password) throw {status: 409};
 }
 
+export async function validateCvc(card: any, cvc: string) {
+    const {securityCode} = card;
+    const isValid = cryptr.decrypt(securityCode) === cvc;
+    if (!isValid) throw {status: 422};
+}
+
 export async function activateCard(id: number, cvc: string, password: string) {
     const card = await validateRegistration(id);
     await validateExpiration(card);
     await validateActivation(card);
+    await validateCvc(card, cvc);
 }
